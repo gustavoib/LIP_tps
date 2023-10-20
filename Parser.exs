fef8629d64b27425dde5087a8f4e7aa9212db589
Gustavo Irineu <101259023+gustavoib@users.noreply.github.com>
2023-10-20 19:06:08 -0300

    Update Parser.exs

--- Parser.exs
+++ Parser.exs
@@ -34,10 +34,10 @@ defmodule Parser do
   def prog(s1) do
     [head1 | s2] = s1
     if head1 == :program do
-      result = id(s2)
-      if elem(result, 0) or not isIdent(elem(result, 1)) do
-        y = elem(result, 1)
-        s3 = elem(result, 2)
+      aux = id(s2)
+      if elem(aux, 0) or not isIdent(elem(aux, 1)) do
+        y = elem(aux, 1)
+        s3 = elem(aux, 2)
         [head2 | s4] = s3
         if head2 == ';' do
           {z, s5} = stat(s4)
@@ -58,26 +58,30 @@ defmodule Parser do
         [head4 | s4] = s3
         if head4 == :then do
           {x1, s5} = stat(s4)
-          [head5 | s6] = s5
+          #[head5 | s6] = s5
+          head5 = hd(s5)
+          s6 = tl(s5)
           if head5 == :else do
-            {x2, s7} = stat(s6)  # Correção aqui
+            {x2, s7} = stat(s6) 
             {NodeState.new(:if, c, x1, x2), s7}
           else
-            {NodeBinario.new(:if, c, x1), s5}  # Correção aqui
+            {NodeBinario.new(:if, c, x1), s5}  
           end
         end
 
       :while ->
         {c, s3} = comp(s2)
-        [head6 | s4] = s3
+        #[head6 | s4] = s3
+        head6 = hd(s3)
+        s4 = tl(s3)
         if head6 == :do do
           {x, s5} = stat(s4)
-          {NodeBinario.new(:while, c, x), s5}  # Correção aqui
+          {NodeBinario.new(:while, c, x), s5}  
         end
 
       :read ->
-        {resul, i, s3} = id(s2)
-        if resul do
+        {aux, i, s3} = id(s2)
+        if aux do
           {NodeUnario.new(:read, i), s3}
         end
 
@@ -91,13 +95,10 @@ defmodule Parser do
           if head7 == ':=' do
             {e, s4} = expr(s3)
             {NodeBinario.new(:assing, t, e), s4}
-          else
-            raise "token não identificado"
           end
         end
     end
   end
-
 
   def sequence(nonTerm, sep, s1) do
     {x1, s2} = nonTerm.(s1)
@@ -111,20 +112,25 @@ defmodule Parser do
   end
 
   def comp(s1) do
-    sequence(&expr/1, &cop/1, s1)
+    a = &expr/1
+    b = &cop/1
+    sequence(a, b, s1)
   end
 
   def expr(s1) do
-    sequence(&term/1, &eop/1, s1)
+    a = &term/1
+    b = &eop/1
+    sequence(a, b, s1)
   end
 
   def term(s1) do
-    sequence(&fact/1, &top/1, s1)
+    a = &fact/1
+    b = &top/1
+    sequence(a, b, s1)
   end
 
   def cop(y) do
-    y == '<' or y == '>' or y == '=<' or
-      y == '>' or y == '==' or y == '!='
+    y == '<' or y == '>' or y == '=<' or y == '>' or y == '==' or y == '!='
   end
 
   def eop(y) do
@@ -146,11 +152,7 @@ defmodule Parser do
         [a | sn] = s3
         if a == ')' do
           {e, sn}
-        else
-          raise "entrada incompatível."
         end
-      else
-        raise "entrada incompatível."
       end
     end
   end
@@ -168,12 +170,8 @@ defmodule Parser do
     end
   end
 
-  def isIdent(x) do
-    e_atom(x)
-  end
-
-  def e_atom(id) do
-    Enum.member?([';', :if, :while, :read, :write, ':='], id) or is_atom(id) 
+  def isIdent(id) do
+    Enum.member?([';', :if, :while, :read, :write, ':='], id) or is_atom(id)
   end
 
   #FORMATAÇÃO DA SAÍDA
@@ -200,7 +198,8 @@ defmodule Parser do
 end
 
 #CHAMADA DO PROGRAMA
-programa = [:program, 'foo', ';', :while, :a, '+', 3, :do, :b, ':=', :b, '+', 1, 'end']
+programa = [:program, 'foo', ';', :while, :a, '+', 3, '<', :b, :do, :b, ':=', :b, '+', 1, 'end']
 {syntatic,sn} = Parser.prog(programa)
+#é possível ver a arvore gerada a partir da implementação em elixir, usando esse comando: IO.inspect(syntatic)
 syntatic_tree = Parser.format_tree(syntatic)
 IO.puts(syntatic_tree)
